1f29a9acd7dde504750ad6f9520caa381b0db7da
Elk-Skinned <elkskinn@proton.me>
2026-09-02 01:38:03 -0700

    Test

--- pm_shared/pm_shared.h
+++ pm_shared/pm_shared.h
@@ -27,6 +27,8 @@ char PM_FindTextureType( char *name );
 extern cvar_t sv_bunny;
 extern cvar_t sv_pogo;
 
+// Test for Git
+
 // Spectator Movement modes (stored in pev->iuser1, so the physics code can get at them)
 #define OBS_NONE				0
 #define OBS_CHASE_LOCKED		1
